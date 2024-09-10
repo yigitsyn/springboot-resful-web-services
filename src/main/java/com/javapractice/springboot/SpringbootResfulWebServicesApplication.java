2f8002431c1d7c2f12277f8a39e5a9ec8ce2b3c8
yigit <myigitsyn@gmail.com>
2024-09-11 01:54:18 +0300

    Initial commit

---
+++ src/main/java/com/javapractice/springboot/SpringbootResfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.javapractice.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootResfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootResfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javapractice/springboot/SpringbootResfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javapractice.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootResfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
