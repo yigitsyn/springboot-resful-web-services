68bbeb13989ba95a165cd768aabb5a9fc6d7e470
yigit <myigitsyn@gmail.com>
2024-09-11 04:51:44 +0300

    UserNotFoundException and rest api are added.

--- src/main/java/com/javapractice/springboot/controller/UserController.java
+++ src/main/java/com/javapractice/springboot/controller/UserController.java
@@ -2,12 +2,10 @@ package com.javapractice.springboot.controller;
 
 import com.javapractice.springboot.entity.User;
 import com.javapractice.springboot.service.UserService;
+import jakarta.persistence.GeneratedValue;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users/api")
@@ -21,9 +19,18 @@ public class UserController {
     }
 
     //build create User REST API
+    // http://localhost:8080/users/api
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    //build get user by id RestAPI
+    //http://localhost:8080/users/api/6675b2dd-0d7f-44ee-9c14-22ac107863cb
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable String id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/javapractice/springboot/service/UserService.java
+++ src/main/java/com/javapractice/springboot/service/UserService.java
@@ -2,8 +2,11 @@ package com.javapractice.springboot.service;
 
 
 import com.javapractice.springboot.entity.User;
+import com.javapractice.springboot.exception.UserNotFoundException;
 import com.javapractice.springboot.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +19,10 @@ public class UserService {
 
     public User createUser(User user) {
         return userRepository.save(user);
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("Customer could not be found" + id));
+
     }
 }
