9516dd461b4d44184e4f08418c11295d2462ca56
yigit <myigitsyn@gmail.com>
2024-09-22 23:47:09 +0300

    Swagger dependency added also customize swagger documentation with annotations also are implemented.

--- src/main/java/com/javapractice/springboot/SpringbootResfulWebServicesApplication.java
+++ src/main/java/com/javapractice/springboot/SpringbootResfulWebServicesApplication.java
@@ -1,10 +1,28 @@
 package com.javapractice.springboot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
+
+//Swagger Documentation template
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot Rest API Documentation",
+				description = "Spring Boot Rest API Demo",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Yigit",
+						email = "myigitsyn@gmail.com",
+						url ="https://github.com/yigitsyn/springboot-resful-web-services"
+				)
+		)
+)
 @SpringBootApplication
 public class SpringbootResfulWebServicesApplication {
 
